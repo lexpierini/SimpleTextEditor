62561e23d317b92e93d8307da067c06b0800f0c4
Alex Pierini <alex_pierini@hotmail.com>
2020-06-26 13:15:55 -0400

    First commit.

--- TextEditor.Designer.cs
+++ TextEditor.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleTextEditor
 {
-    partial class Form1
+    partial class TextEditor
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "TextEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Éditeur de texte";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
